59ac981ac7200802952727c90597070f56014339
pystockhub <pystock@outlook.com>
2016-08-17 23:29:08 +0900

    update sell_list and buy_list
    Add the functionality to sell and buy

--- ch18/week04/pytrader.py
+++ ch18/week04/pytrader.py
@@ -35,14 +35,70 @@ class MyWindow(QMainWindow, form_class):
         self.pushButton.clicked.connect(self.send_order)
         self.pushButton_2.clicked.connect(self.check_balance)
 
+        self.conduct_buy_sell()
         self.load_buy_sell_list()
 
-    def load_buy_sell_list(self):
-        f = open("buy_list.txt", 'rt') #, encoding="UTF8")
+    def conduct_buy_sell(self):
+        hoga_lookup = {'지정가': "00", '시장가': "03"}
+
+        f = open("buy_list.txt", 'rt')
         buy_list = f.readlines()
         f.close()
 
-        f = open("sell_list.txt", 'rt') #, encoding="UTF8")
+        f = open("sell_list.txt", 'rt')
+        sell_list = f.readlines()
+        f.close()
+
+        account = self.comboBox.currentText()
+
+        # buy list
+        for row_data in buy_list:
+            split_row_data = row_data.split(';')
+            hoga    = split_row_data[2]
+            code    = split_row_data[1]
+            num     = split_row_data[3]
+            price   = split_row_data[4]
+
+            if split_row_data[-1].rstrip() == '매수전':
+                self.kiwoom.SendOrder("SendOrder_req", "0101", account, 1, code, num, price, hoga_lookup[hoga], "")
+
+        # sell list
+        for row_data in sell_list:
+            split_row_data = row_data.split(';')
+            hoga    = split_row_data[2]
+            code    = split_row_data[1]
+            num     = split_row_data[3]
+            price   = split_row_data[4]
+
+            if split_row_data[-1].rstrip() == '매도전':
+                self.kiwoom.SendOrder("SendOrder_req", "0101", account, 2, code, num, price, hoga_lookup[hoga], "")
+
+        # buy list
+        for i, row_data in enumerate(buy_list):
+             buy_list[i] = buy_list[i].replace("매수전", "주문완료")
+
+        # file update
+        f = open("buy_list.txt", 'wt')
+        for row_data in buy_list:
+            f.write(row_data)
+        f.close()
+
+        # sell list
+        for i, row_data in enumerate(sell_list):
+             sell_list[i] = sell_list[i].replace("매도전", "주문완료")
+
+        # file update
+        f = open("sell_list.txt", 'wt')
+        for row_data in sell_list:
+            f.write(row_data)
+        f.close()
+
+    def load_buy_sell_list(self):
+        f = open("buy_list.txt", 'rt')
+        buy_list = f.readlines()
+        f.close()
+
+        f = open("sell_list.txt", 'rt')
         sell_list = f.readlines()
         f.close()
 
@@ -54,7 +110,11 @@ class MyWindow(QMainWindow, form_class):
             row_data = buy_list[j]
             split_row_data = row_data.split(';')
             for i in range(len(split_row_data)):
-                item = QTableWidgetItem(split_row_data[i].rstrip())
+                if i == 1:
+                    name = self.kiwoom.GetMasterCodeName(split_row_data[i].rstrip())
+                    item = QTableWidgetItem(name)
+                else:
+                    item = QTableWidgetItem(split_row_data[i].rstrip())
                 item.setTextAlignment(Qt.AlignVCenter | Qt.AlignCenter)
                 self.tableWidget_4.setItem(j, i, item)
 
@@ -63,7 +123,11 @@ class MyWindow(QMainWindow, form_class):
             row_data = sell_list[j]
             split_row_data = row_data.split(';')
             for i in range(len(split_row_data)):
-                item = QTableWidgetItem(split_row_data[i].rstrip())
+                if i == 1:
+                    name = self.kiwoom.GetMasterCodeName(split_row_data[i].rstrip())
+                    item = QTableWidgetItem(name)
+                else:
+                    item = QTableWidgetItem(split_row_data[i].rstrip())
                 item.setTextAlignment(Qt.AlignVCenter | Qt.AlignCenter)
                 self.tableWidget_4.setItem(len(buy_list) + j, i, item)
 
