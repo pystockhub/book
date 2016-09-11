7241f166be7069b75d567c5c409029cac059e054
pystock <pystock@outlook.com>
2016-09-11 21:41:48 +0900

    week06
    initial version

---
+++ ch19/week06/pymon.py
@@ -0,0 +1,141 @@
+import sys
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+from PyQt4.QAxContainer import *
+import Kiwoom
+import pandas as pd
+import datetime
+import time
+import webreader
+
+MARKET_KOSPI   = 0
+MARKET_KOSDAK  = 10
+
+class PyMon:
+    def __init__(self):
+        self.kiwoom = Kiwoom.Kiwoom()
+        self.kiwoom.CommConnect()
+        self.get_code_list()
+
+    def get_code_list(self):
+        self.kospi_codes = self.kiwoom.GetCodeListByMarket(MARKET_KOSPI)
+        self.kosdak_codes = self.kiwoom.GetCodeListByMarket(MARKET_KOSDAK)
+
+    def get_ohlcv(self, code, start_date):
+        # Init data structure
+        self.kiwoom.InitOHLCRawData()
+
+        # Request TR and get data
+        self.kiwoom.SetInputValue("종목코드", code)
+        self.kiwoom.SetInputValue("기준일자", start_date)
+        self.kiwoom.SetInputValue("수정주가구분", 1)
+        self.kiwoom.CommRqData("opt10081_req", "opt10081", 0, "0101")
+        time.sleep(0.2)
+
+        # DataFrame
+        df = pd.DataFrame(self.kiwoom.ohlcv, columns=['open', 'high', 'low', 'close', 'volume'],
+                          index=self.kiwoom.ohlcv['date'])
+        return df
+
+    def check_speedy_rising_volumn(self, code):
+        today = datetime.datetime.today().strftime("%Y%m%d")
+        df = self.get_ohlcv(code, today)
+        volumes = df['volume']
+
+        sum_vol20 = 0
+        avg_vol20 = 0
+
+        # Check small trading days
+        if len(volumes) < 21:
+            return False
+
+        # Accumulation
+        for i, vol in enumerate(volumes):
+            if i == 0:
+                today_vol = vol
+            elif i >= 1 and i <= 20:
+                sum_vol20 += vol
+            elif i >= 21:
+                break
+
+        #  Average and decision
+        avg_vol20 = sum_vol20 / 20;
+        if today_vol > avg_vol20 * 10:
+            return True
+        else:
+            return False
+
+    def update_buy_list(self, buy_list):
+        f = open("buy_list.txt", "wt")
+        for code in buy_list:
+            f.writelines("매수;%s;시장가;10;0;매수전\n" % (code))
+        f.close()
+
+    def run(self):
+        buy_list = []
+
+        for code in self.kospi_codes:
+            if self.check_speedy_rising_volumn(code):
+                buy_list.append(code)
+
+        for code in self.kosdak_codes:
+            if self.check_speedy_rising_volumn(code):
+                buy_list.append(code)
+
+        self.update_buy_list(buy_list)
+
+    def calculate_estimated_dividend_to_treasury(self, code):
+        estimated_dividend_yield = float(webreader.get_estimated_dividend_yield(code))
+        current_3year_treasury = float(webreader.get_current_3year_treasury())
+        estimated_dividend_to_treasury = estimated_dividend_yield / current_3year_treasury
+        return estimated_dividend_to_treasury
+
+    def get_min_max_dividend_to_treasury(self, code):
+        previous_dividend_yield = webreader.get_previous_dividend_yield(code)
+        three_years_treasury = webreader.get_3year_treasury()
+
+        now = datetime.datetime.now()
+        cur_year = now.year
+        previous_dividend_to_treasury = {}
+
+        for year in range(cur_year-5, cur_year):
+            if year in previous_dividend_yield.keys() and year in three_years_treasury.keys():
+                ratio = float(previous_dividend_yield[year]) / float(three_years_treasury[year])
+                previous_dividend_to_treasury[year] = ratio
+
+        print(previous_dividend_to_treasury)
+        min_ratio = min(previous_dividend_to_treasury.values())
+        max_ratio = max(previous_dividend_to_treasury.values())
+
+        return (min_ratio, max_ratio)
+
+    def check_dividend_algorithm(self, code):
+        estimated_dividend_to_treasury = self.calculate_estimated_dividend_to_treasury(code)
+        (min_ratio, max_ratio) = self.get_min_max_dividend_to_treasury(code)
+
+        if estimated_dividend_to_treasury >= max_ratio:
+            return (1, estimated_dividend_to_treasury)
+        elif estimated_dividend_to_treasury <= min_ratio:
+            return (-1, estimated_dividend_to_treasury)
+        else:
+            return (0, estimated_dividend_to_treasury)
+
+    def run_dividend(self):
+        buy_list = []
+
+        for code in self.kospi_codes:
+            ret = self.check_dividend_algorithm(code)
+            if ret[0] == 1:
+                buy_list.append((code, ret[1]))
+
+        for code in self.kosdak_codes:
+            ret = self.check_dividend_algorithm(code)
+            if ret[0] == 1:
+                buy_list.append((code, ret[1]))
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    pymon = PyMon()
+    #pymon.run()
+    #print(pymon.calculate_estimated_dividend_to_treasury('058470'))
+    print(pymon.get_min_max_dividend_to_treasury('058470'))
---
+++ ch19/week06/webreader.py
@@ -0,0 +1,102 @@
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+import datetime
+
+def get_financial_statements(code):
+    url = "http://companyinfo.stock.naver.com/v1/company/ajax/cF1001.aspx?cmp_cd=%s&fin_typ=0&freq_typ=Y" % (code)
+    html = requests.get(url).text
+
+    html = html.replace('<th class="bg r01c02 endLine line-bottom"colspan="8">연간</th>', "")
+    html = html.replace("<span class='span-sub'>(IFRS연결)</span>", "")
+    html = html.replace("<span class='span-sub'>(IFRS별도)</span>", "")
+    html = html.replace('\t', '')
+    html = html.replace('\n', '')
+    html = html.replace('\r', '')
+
+    html = html.replace('2011/12', '2011')
+    html = html.replace('2012/03', '2011')
+    html = html.replace('2012/12', '2012')
+    html = html.replace('2013/03', '2012')
+    html = html.replace('2013/12', '2013')
+    html = html.replace('2014/03', '2013')
+    html = html.replace('2014/12', '2014')
+    html = html.replace('2015/03', '2014')
+    html = html.replace('2015/12', '2015')
+
+    df_list = pd.read_html(html, index_col='주요재무정보')
+    df = df_list[0]
+    return df
+
+def get_3year_treasury():
+    url = "http://www.index.go.kr/strata/jsp/showStblGams3.jsp?stts_cd=107301&idx_cd=1073"
+    html = requests.get(url).text
+
+    soup = BeautifulSoup(html, 'lxml')
+    tr_data = soup.find_all('tr', id='tr_107301_1')
+    td_data = tr_data[0].find_all('td')
+
+    treasury_3year = {}
+    start_year = 1997
+
+    for x in td_data:
+        treasury_3year[start_year] = x.text
+        start_year += 1
+
+    return treasury_3year
+
+def get_dividend_yield(code):
+    url = "http://companyinfo.stock.naver.com/company/c1010001.aspx?cmp_cd=" + code
+    html = requests.get(url).text
+
+    soup = BeautifulSoup(html, 'lxml')
+    td_data = soup.find_all('td', {'class': 'cmp-table-cell td0301'})
+    dt_data = td_data[0].find_all('dt')
+
+    dividend_yield = dt_data[5].text
+    dividend_yield = dividend_yield.split(' ')[1]
+    dividend_yield = dividend_yield[:-1]
+    return dividend_yield
+
+def get_estimated_dividend_yield(code):
+    df = get_financial_statements(code)
+
+    now = datetime.datetime.now()
+    cur_year = '%d/12(E)' % (now.year)
+    cur_year = df[cur_year]
+
+    estimated_dividend_yield = cur_year['현금배당수익률']
+
+    return estimated_dividend_yield
+
+def get_previous_dividend_yield(code):
+    df = get_financial_statements(code)
+    dividend_yields = df.ix['현금배당수익률']
+
+    ret = {}
+    now = datetime.datetime.now()
+    cur_year = now.year
+
+    for year in range(cur_year-5, cur_year):
+        if str(year) in dividend_yields.index:
+            ret[year] = dividend_yields[str(year)]
+
+    return ret
+
+def get_current_3year_treasury():
+    url = "http://info.finance.naver.com/marketindex/interestDailyQuote.nhn?marketindexCd=IRR_GOVT03Y&page=1"
+    html = requests.get(url).text
+
+    soup = BeautifulSoup(html, 'lxml')
+    tbody_data = soup.find_all('tbody')
+    tr_data = tbody_data[0].find_all('tr')
+    td_data = tr_data[0].find_all('td')
+    return td_data[1].text
+
+if __name__ == "__main__":
+    #print(get_dividend_yield('058470'))
+    #print(get_estimated_dividend_yield('058470'))
+    #print(get_current_3year_treasury())
+    print(get_previous_dividend_yield('058470'))
+
+
