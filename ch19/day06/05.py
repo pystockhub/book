7bf562c98aeeb2dee1e30aef062fc983eea11428
Rae Woong Park <rwpark99@gmail.com>
2020-02-21 22:08:13 +0900

    debug CERTIFICATE_VERIFY_FAILED
    , verify=False) added

--- ch19/day06/05.py
+++ ch19/day06/05.py
@@ -1,6 +1,6 @@
 import requests
 
-response = requests.get("https://companyinfo.stock.naver.com/v1/company/c1010001.aspx?cmp_cd=035720")
+response = requests.get("https://companyinfo.stock.naver.com/v1/company/c1010001.aspx?cmp_cd=035720", verify=False)
 print(response.text)
 
 
